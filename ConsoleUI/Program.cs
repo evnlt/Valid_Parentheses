fc2a30fd3e4b5e520a3a63447ab44237a621771f
Ivan Lutsenko <evanlt187@gmail.com>
2022-10-17 22:55:08 +0300

    refactor

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,82 +9,32 @@ namespace ConsoleUI
             Console.ForegroundColor = ConsoleColor.White;
             while (true)
             {
-                Console.Write("Enter string: ");
+                Console.Write("Enter a string: ");
                 string input = Console.ReadLine();
 
                 try
                 {
                     Console.WriteLine("Result: " + Solution.IsValid(input));
                 }
-                catch (Exception)
+                catch (ArgumentException)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Invalid input");
                     Console.ForegroundColor= ConsoleColor.White;
                 }
+                catch (Exception) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("An error has occured");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
 
                 Console.WriteLine("\nPress 'y' to restart.");
-                if (Console.ReadKey().Key != ConsoleKey.Y)
-                    return;
-                else
+                if (Console.ReadKey().Key == ConsoleKey.Y)
                     Console.Clear();
+                else
+                    return;
             }
-        }
-    }
-
-
-
-
-
-
-
-
-    public static class Solution
-    {
-        private static Dictionary<char, char> _brackets = new() { { '(', ')' }, { '{', '}' }, { '[', ']' } };
-
-        // O(n)
-        public static bool IsValid(string str)
-        {
-            if (str == null)
-                throw new ArgumentNullException("String is null");
-
-            int len = str.Length;
-            if (len <= 1 || len > 1000)
-                throw new ArgumentException("Invalid length");
-            if (!str.ContainsOnlyParenthesis())
-                throw new ArgumentException("Not all characters are parenthesis");
-
-            if (len % 2 == 1)
-                return false;
-
-            for (int i = 0; i < len - 1; i += 2)
-            {
-                foreach (var pair in _brackets)
-                {
-                    if (str[i] == pair.Key) // is an opening bracket
-                    {
-                        if (str[i + 1] != pair.Value) // is a closing bracket
-                            return false;
-                        break;
-                    }
-                    else if (str[i] == pair.Value) // is a closing bracket
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
-        private static bool ContainsOnlyParenthesis(this string str)
-        {
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (!(_brackets.ContainsKey(str[i]) || _brackets.ContainsValue(str[i])))
-                    return false;
-            }
-            return true;
         }
     }
 }
---
+++ ConsoleUI/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class Solution
+    {
+        private static Dictionary<char, char> _brackets = new() { { '(', ')' }, { '{', '}' }, { '[', ']' } };
+
+        // O(n) despite there being 2 (not nested) for loops
+
+        public static bool IsValid(string str)
+        {
+            if (str == null)
+                throw new NullReferenceException();
+
+            int len = str.Length;
+            if (len <= 1 || len > 1000)
+                throw new ArgumentException("Invalid length");
+            if (!str.ContainsOnlyParenthesis())
+                throw new ArgumentException("Not all characters are parenthesis");
+
+            if (len % 2 == 1)
+                return false;
+
+            for (int i = 0; i < len - 1; i += 2)
+            {
+                foreach (var pair in _brackets)
+                {
+                    if (str[i] == pair.Key) // is an opening bracket
+                    {
+                        if (str[i + 1] != pair.Value) // is a closing bracket
+                            return false;
+                        break;
+                    }
+                    else if (str[i] == pair.Value) // is a closing bracket
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        private static bool ContainsOnlyParenthesis(this string str)
+        {
+            foreach (char c in str)
+            {
+                if (!(_brackets.ContainsKey(c) || _brackets.ContainsValue(c)))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
